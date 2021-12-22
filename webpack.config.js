049ba3164e3b1730985c7236848d5b4da38bcac3
marlonaesparza <marlonaesparza@gmail.com>
2021-12-22 09:33:33 -0800

    bpm/overview.dio: Done designing the goals component overview.

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  mode: 'development',
+  entry: __dirname + '/client/index.js',
+  output: {
+    path: __dirname + '/public/dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: __dirname + '/node_modules',
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react', '@babel/preset-env'],
+              plugins: ['babel-plugin-styled-components']
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.js', '.jsx']
+  }
+};
